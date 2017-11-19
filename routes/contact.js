fb537203c6b196f4d6f3c7ea281c60c8c897f13e
pankajnarola1995 <pankajnarola1995@gmail.com>
2017-11-19 21:00:05 +0100

    contact file inserted

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongoose');
+var assert = require('assert');
+var app = express();
+
+var url = 'mongodb://localhost:27017/StudyConDb';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('contact');
 });
+
+router.get('/get-data',function (req,res,next) {
+    var resultArray = [];
+    mongo.connect(url,function (err,db) {
+        assert.equal(null,err);
+       var cursor = db.collection('contact').find();
+       cursor.forEach(function(doc,err){
+           assert.equal(null,err);
+           resultArray.push(doc);
+       }, function () {
+            db.close();
+            res.render('contact',{items:resultArray});
+       } );
+    });
+});
+router.post('/insert',function (req,res,next) {
+
+});
+
+
+
 
 module.exports = router;
