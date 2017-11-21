28faa21942ec340b3c6765e373ccab14cc16f536
Khushbu-Vadadoriya <khushbuvadadoriya3@gmail.com>
2017-11-21 12:20:40 +0100

    change

--- app.js
+++ app.js
@@ -5,50 +5,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var assert = require('assert');
-
-//mongodb url and required
-//var url = 'mongodb://localhost:27017/StudyConDb';
 const mongoose= require('mongoose');
-var dbConn = mongoose.connect('mongodb://localhost/StudyConDb', {
-    useMongoClient: true,
-    /* other options */
-});
-if (dbConn){
-    console.log('Database connection success');
-} else {
-    console.log('Database connection fail');
-}
-/*
-var user = mongoose.model('emp',schema);
-
-var schema= new mongoose.Schema({
-  age:'number'
-});
-
-*/
-
-app.use(bodyParser.urlencoded({extended:false}));
-
-
-/*app.post('/post-contact', function (req,res) {
-  //req.end("stop");
-    var dbConn = mongoose.connect('mongodb://localhost/myapp', {
-        useMongoClient: true,
-        /* other options
-
-    });
-        delete req.body._id; // for safety reasons
-        dbConn.collection('contact').insertOne(req.body);
-=======
-
->>>>>>> d29253303279b770272f704fa718c1daa6cfbbd5
-
+var assert = require('assert');
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//database connection
+var dbConn = mongoose.connect('mongodb://localhost/myapp', {
+    useMongoClient: true,
+    /* other options */
+});
+if(dbConn) {
+    console.log('success');
+}else {
+    console.log('fail');
+}
+
 
 
 // uncomment after placing your favicon in /public
@@ -65,57 +40,47 @@ app.use('/users', require('./routes/users'));
 app.use('/home', require('./routes/home'));
 app.use('/contact', require('./routes/contact'));
 
+var url = 'mongodb://localhost:27017/StudyConDb';
 
-var contactSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    subject: String,
-    message:String
+app.post('/post-contact', function (req,res) {
 
-});
-var contact = mongoose.model('contact',contactSchema);
-
-app.post('/save_contact', function (req,res) {
-    var contactinfo = new contact({
+//console.log("postcontact");
+    var item = {
         name: req.body.name,
-        email: req.body.email,
-        subject: req.body.subject,
-        message: req.body.message
+        age: req.body.age
+    };
+    mongoose.connect(url, function (err,db) {
+        //useMongoClient: true,
+        assert.equal(null,err);
+        db.collection('contact').insertOne(item, function (err,result) {
+            assert.equal(null,error);
+            console.log('data insrrtes');
+            db.clone();
+
+        });
 
     });
+    res.send('/insert');
 
-    contactinfo.save(function (err, thro) {
-        if (err) {
-            return console.error(err);
-        }
-        else {
-            console.log("1 record inserted");
-
-
-            //alert("Your data sent successfully! We will get back to you soon... ");
-            res.redirect('/');
-        }
-    });
 });
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
     //database connection
     //mongoose.connect('mongodb:http://localhost:27017/mongo');
 
     // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
