90462e74c9d8f902634a6a38fd5a672f734a94dc
pankajnarola1995 <pankajnarola1995@gmail.com>
2017-12-10 21:18:49 +0100

    Admin Side Consultancy Adding Single And Multiple Image and Delete data

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-const mongoose= require('mongoose');
-var assert = require('assert');
+let express = require('express');
+let app = express();
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let assert = require('assert');
 const fileUpload = require('express-fileupload');
 
-// this is task4
-//These are some changes for task 3
-
 //database connection
-var dbConn = mongoose.connect('mongodb://localhost/StudyConDb', {
+let dbConn = mongoose.connect('mongodb://localhost/StudyConDb', {
     useMongoClient: true,
     /* other options */
 });
-if(dbConn) {
+if (dbConn) {
     console.log('success');
-}else {
+} else {
     console.log('fail');
 }
 
@@ -31,7 +29,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -47,13 +45,14 @@ app.use('/register', require('./routes/register'));
 app.use('/Admin', require('./routes/Admin/AdminIndex'));
 app.use('/Admin/AdminContactView', require('./routes/Admin/AdminContactView'));
 app.use('/AdminConsultancy', require('./routes/Admin/AdminConsultancy'));
+app.use('/AdminConsultancyAdddata', require('./routes/Admin/AdminConsultancy'));
 app.use('/Admin/AdminLanguage', require('./routes/Admin/AdminLanguage'));
-app.use('/Admin/AdminAddEvent', require('./routes/Admin/AdminAddEvent'));
+app.use('/AdminAddEvent', require('./routes/Admin/AdminAddEvent'));
 
 app.use(fileUpload());
 
 //contact table create
-var contactSchema =  mongoose.Schema({
+let contactSchema = mongoose.Schema({
 
     Name: String,
     Subject: String,
@@ -61,33 +60,48 @@ var contactSchema =  mongoose.Schema({
     Message: String
 
 });
-var contact = mongoose.model("contact",contactSchema);
+//Consultancy Table Create
+let ConsultancySchema = mongoose.Schema({
+    _id: Schema.Types.ObjectId,
+    country_name: String,
+    flage_image: String,
+    requirenment: String,
+    detail: String,
+    important_link: String
+
+});
+//Image Table Create
+let ImageSchema = mongoose.Schema({
+    //author: { type: Schema.Types.ObjectId, ref: 'Person' },
+    consultancy_id: {type: Schema.Types.ObjectId, ref: 'Consultancy'},
+    images_name: String,
+
+});
+let Image = mongoose.model("Image", ImageSchema);
+let Consultancy = mongoose.model("Consultancy", ConsultancySchema);
+let contact = mongoose.model("contact", contactSchema);
 
 //contact method call
-app.post('/saveContact', function (req,res) {
+app.post('/saveContact', function (req, res) {
     console.log("postcontact");
-    var contactdata = new contact( {
+    let contactdata = new contact({
         Name: req.body.name,
         Subject: req.body.subject,
-        Email:req.body.email,
-        Message:req.body.message
+        Email: req.body.email,
+        Message: req.body.message
     });
-        contactdata.save(function(error, data){
-            if(error){
-                console.log("contact insert error ");
-                res.json(error);
+    contactdata.save(function (error, data) {
+        if (error) {
+            console.log("contact insert error ");
+            res.json(error);
 
-            }
-            else{
-                console.log("contact inserted ");
-                res.redirect("/");
-            }
-        });
+        }
+        else {
+            console.log("contact inserted ");
+            res.redirect("/");
+        }
     });
-
-
-
-
+});
 
 
 /*
@@ -109,101 +123,153 @@ app.get('/AdminContactView',function (req,res) {
 
 
 });*/
-var ConsultancySchema =  mongoose.Schema({
-
-    country_name: String,
-    flage_image: String,
-    requirenment:String,
-    detail:String,
-    important_link:String,
-    images:String
-
-});
-var Consultancy = mongoose.model("Consultancy",ConsultancySchema);
-
 
 //Consultancy Insert data
-app.post('/AdminConsultancyAddData', function (req,res) {
-    console.log("AdminConsultancyAddData");
+/*
+app.post('/AdminConsultancyAddData', function (req, res) {
 
-      if (!req.files)
+    let images = req.files.images.length;
+    console.log(images);
+    let newpath = new Array();
+
+    for (let i = 0; i < req.files.images.length; i++) {
+
+        newpath[i] = './public\\images\\' + images[i].name;
+        console.log(newpath[i]);
+        images[i].mv(newpath[i], function (err) {
+            let image_name = images[i].name;
+            let imagedata = new  Image({
+                images_name: image_name,
+            });
+            imagedata.save(function (error, data) {
+                if (error) {
+                    console.log("image insert error ");
+                    res.json(error);
+
+                }
+                else {
+                    console.log("image inserted ");
+                }
+            });
+
+            console.log('File uploaded Flage Image Consultancy!');
+
+
+        });
+    };
+    res.redirect("/AdminConsultancy");
+
+
+    //console.log(req.files.images);
+
+
+    //res.send(req.files.name);
+
+});
+*/
+app.post('/AdminConsultancyAddData', function (req, res) {
+    if (!req.files)
         return res.status(400).send('No files were uploaded.');
 
-    var flage_image1 = req.files.flage_image;
-    console.log(flage_image1);
-    // Use the mv() method to place the file somewhere on your server
-    var newpath = './public\\images\\' + flage_image1.name;
-    flage_image1.mv(newpath  , function(err) {
+    let images = req.files.images;
+    let flage_image1 = req.files.flage_image;
+    let mul_newpath = new Array();
+    let newpath = './public\\images\\Consultancy_Flag\\' + flage_image1.name;
+
+    flage_image1.mv(newpath, function (err) {
         if (err)
             return res.status(500).send(err);
 
-        console.log('File uploaded Flage Image Consultancy!');
+        console.log('File uploaded: Flage Image Consultancy!');
     });
-
-
-
-
-    var consultancyData = new Consultancy( {
-        country_name:    req.body.country_name,
-        flage_image:     flage_image1.name,
-        requirenment:    req.body.requirenment,
-        detail:        req.body.detail,
+//Consultancy Data Inserted
+    let consultancyData = new Consultancy({
+        _id: new mongoose.Types.ObjectId(),
+        country_name: req.body.country_name,
+        flage_image: flage_image1.name,
+        requirenment: req.body.requirenment,
+        detail: req.body.detail,
         important_link: req.body.important_link,
-        images:         images.name
     });
     console.log(consultancyData);
 
-    var promise = consultancyData.save();
+    let promise = consultancyData.save();
     assert.ok(promise instanceof require('mpromise'));
+    promise.then(function (result) {
+        console.log("inserted Consultancy data"); // "Stuff worked!"
+    }, function (err) {
+        console.log(err); // Error: "It broke"
+    });
 
-    if(promise) {
-        console.log("inserted country data");
-        res.redirect("/AdminConsultancy");
-    }
-    else {
-        console.log("error in insert country");
-        res.redirect("/AdminConsultancy");
+// For loop For Multiple File Uploading
+    for (let i = 0; i < req.files.images.length; i++) {
+        mul_newpath[i] = './public\\images\\Consultancy\\' + images[i].name;
+        console.log(mul_newpath[i]);
+        images[i].mv(mul_newpath[i], function (err) {
 
-    }
-});
-//Consultancy Delete data
-app.post('/AdminConsultancyDeleteData',function (req,res) {
+            let image_name = images[i].name;
+            let imagedata = new Image({
+                consultancy_id: consultancyData._id,    // assign the _id from the person
+                images_name: image_name,
+            });
+            imagedata.save(function (error, data) {
+                if (error) {
+                    console.log("image insert error ");
+                    res.json(error);
 
-        var cid = req.body.cid;
-        console.log(cid);
-        Consultancy.remove({_id: cid}, function (err) {
-            if (err) {
-                res.json({"err": err});
-            } else {
-                res.json({success: true});
-            }
-
+                }
+                else {
+                    console.log("Multiple image inserted " + [i]);
+                }
+            });
         });
-        //Consultancy.findByIdAndRemove(cid).then((docs) => {});
+    };
+    console.log("Everything Done");
+    res.redirect("/AdminConsultancy");
 
-        //Consultancy.delete(function(err,Consultancy){
-        // if(err) throw err;
-        // console.log('the document is deleted');
-        //res.send(question);
 
-        //});
+});
+
+//Consultancy Delete data
+app.post('/AdminConsultancyDeleteData', function (req, res) {
+
+    let cid = req.body.cid;
+    console.log(cid);
+    Consultancy.remove({_id: cid}, function (err) {
+        if (err) {
+            res.json({"err": err});
+        } else {
+            Image.remove({consultancy_id: cid}, function (err) {
+                res.json({success: true});
+            });
+        }
+
+    });
+    //Consultancy.findByIdAndRemove(cid).then((docs) => {});
+
+    //Consultancy.delete(function(err,Consultancy){
+    // if(err) throw err;
+    // console.log('the document is deleted');
+    //res.send(question);
+
+    //});
 
 
 });
 
 //Consultancy Update data
-app.post('/AdminConsultancyUpdateGetData',function (req,res) {
+app.post('/AdminConsultancyUpdateGetData', function (req, res) {
     console.log("Ajax working");
 
 
-    var cid = req.body.cid;
+    let cid = req.body.cid;
     console.log(cid);
-    Consultancy.find({_id: cid}, function (err,data) {
+    Consultancy.find({_id: cid}, function (err, data) {
         if (err) {
             res.json({"err": err});
         } else {
             //console.log(data);
-            res.send({Consultancy:data});
+            res.send({Consultancy: data});
         }
 
     });
@@ -211,19 +277,19 @@ app.post('/AdminConsultancyUpdateGetData',function (req,res) {
 
 
 //Consultancy Update data
-app.post('/AdminConsultancyUpdateData', function (req,res) {
+app.post('/AdminConsultancyUpdateData', function (req, res) {
     console.log("AdminConsultancyAddData");
 
 
-    var country_name = req.body.country_name;
-    var flage_image = req.body.flage_image;
-    var requirenment = req.body.requirenment;
-    var detail = req.body.detail;
-    var important_link = req.body.important_link;
+    let country_name = req.body.country_name;
+    let flage_image = req.body.flage_image;
+    let requirenment = req.body.requirenment;
+    let detail = req.body.detail;
+    let important_link = req.body.important_link;
     //   images:         req.body.images
 
     // console.log(consultancyData);
-    var promise = Consultancy.update({'country_name': country_name},
+    let promise = Consultancy.update({'country_name': country_name},
         {
             $set: {
                 'country_name': country_name,
@@ -246,38 +312,38 @@ app.post('/AdminConsultancyUpdateData', function (req,res) {
 });
 
 
-var AddEventSchema =  mongoose.Schema({
+let AddEventSchema = mongoose.Schema({
 
     event_name: String,
     event_description: String,
-    event_type:String,
-    event_details:String,
-    images:String,
+    event_type: String,
+    event_details: String,
+    images: String,
     // images:String
 
 });
 
 //Admin Event handling
 
-var AddEvent = mongoose.model("AddEvent",ConsultancySchema);
+let AddEvent = mongoose.model("AddEvent", ConsultancySchema);
 
 
-app.post('/AdminAddEventAddData', function (req,res) {
+app.post('/AdminAddEventAddData', function (req, res) {
     console.log("AdminAddEventAddData");
 
-    var AddEventData = new AddEvent( {
-        event_name:    req.body.event_name,
-        event_description:     req.body.event_description,
-        event_type:    req.body.event_type,
-        event_details:        req.body.event_details,
+    let AddEventData = new AddEvent({
+        event_name: req.body.event_name,
+        event_description: req.body.event_description,
+        event_type: req.body.event_type,
+        event_details: req.body.event_details,
         images: req.body.images,
         //   images:         req.body.images
     });
     console.log(AddEventData);
-    var promise = AddEventData.save();
+    let promise = AddEventData.save();
     assert.ok(promise instanceof require('mpromise'));
 
-    if(promise) {
+    if (promise) {
         console.log("inserted event data");
         res.redirect("/AdminAddEvent");
     }
@@ -288,13 +354,11 @@ app.post('/AdminAddEventAddData', function (req,res) {
     }
 
 
-
-
 });
 
 
-app.get('/ajaxcall', function (req,res) {
-    contact.find(function (err,data) {
+app.get('/ajaxcall', function (req, res) {
+    contact.find(function (err, data) {
         if (data) {
             console.log("Contact Data Fetched");
             res.send(data);
@@ -308,16 +372,15 @@ app.get('/ajaxcall', function (req,res) {
 });
 
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -328,8 +391,6 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-
-
 
 
 module.exports = app;
--- routes/Admin/AdminConsultancy.js
+++ routes/Admin/AdminConsultancy.js
@@ -29,6 +29,49 @@ router.get('/', function(req, res, next) {
     });
 
 
+//Consultancy Insert data
+  /*  router.post('/AdminConsultancyAddData', function (req, res) {
+        console.log("AdminConsultancyAddData");
+
+        if (!req.files)
+            return res.status(400).send('No files were uploaded.');
+
+        var flage_image1 = req.files.flage_image;
+        console.log(flage_image1);
+        // Use the mv() method to place the file somewhere on your server
+        var newpath = './public\\images\\' + flage_image1.name;
+        flage_image1.mv(newpath, function (err) {
+            if (err)
+                return res.status(500).send(err);
+
+            console.log('File uploaded Flage Image Consultancy!');
+        });
+
+
+        var consultancyData = new Consultancy({
+            country_name: req.body.country_name,
+            flage_image: flage_image1.name,
+            requirenment: req.body.requirenment,
+            detail: req.body.detail,
+            important_link: req.body.important_link,
+            images: images.name
+        });
+        console.log(consultancyData);
+
+        var promise = consultancyData.save();
+        assert.ok(promise instanceof require('mpromise'));
+
+        if (promise) {
+            console.log("inserted country data");
+            res.redirect("/AdminConsultancy");
+        }
+        else {
+            console.log("error in insert country");
+            res.redirect("/AdminConsultancy");
+
+        }
+    });
+*/
 
 
 });
